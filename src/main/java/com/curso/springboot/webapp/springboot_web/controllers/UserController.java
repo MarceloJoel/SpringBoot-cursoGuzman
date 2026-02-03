5d20498386f99a3498e553c9ef44e8dcf5117bd5
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-02-03 17:19:29 -0500

    fear: listas en thymeleaf

--- src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,19 +1,35 @@
 package com.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.curso.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(Model model){
+    public String details(Model model) {
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("name", "Marcelo");
         model.addAttribute("lastname", "Rodriguez");
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios!");
+
+        return "list";
     }
 
 }
