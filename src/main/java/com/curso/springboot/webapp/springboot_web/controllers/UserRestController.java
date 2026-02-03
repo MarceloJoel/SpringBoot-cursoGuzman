daa71739d006ced71c29666f3ce5f182c6f0a3df
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-02-03 12:04:23 -0500

    feat: apiRest listar

--- src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,15 +1,18 @@
 package com.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.springboot.webapp.springboot_web.models.User;
 import com.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
@@ -17,7 +20,7 @@ public class UserRestController {
 
     @GetMapping("/details")
     public UserDto details() {
-        
+
         User user = new User("Marcelo", "Rodriguez");
 
         UserDto userDto = new UserDto();
@@ -27,12 +30,27 @@ public class UserRestController {
         return userDto;
     }
 
-    @GetMapping(path="/details-map")
+    @GetMapping("/list")
+    public List<User> list() {
+        User user = new User("Andres", "Guzman");
+        User user2 = new User("Pepe", "Dodp");
+        User user3 = new User("Marcelo", "Rodriguez");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
+    }
+
+    @GetMapping(path = "/details-map")
     public Map<String, Object> detailsMap() {
 
         User user = new User("Marcelo", "Rodriguez");
         Map<String, Object> body = new HashMap<>();
-        
+
         body.put("title", "Hola Mundo Spring Boot");
         body.put("user", user);
         return body;
