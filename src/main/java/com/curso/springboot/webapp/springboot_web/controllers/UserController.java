eeb17bc80c5d5bba38aa27ee25eb6b90414b446c
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-02-03 17:59:48 -0500

    feat: foreach thymeleaf

--- src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -9,22 +9,28 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.curso.springboot.webapp.springboot_web.models.User;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model) {
+
+        User user = new User("Marcelo", "Rodriguez");
+        user.setEmail("marcelo@correo.com");
         model.addAttribute("title", "Hola Mundo Spring Boot");
-        model.addAttribute("name", "Marcelo");
-        model.addAttribute("lastname", "Rodriguez");
+        model.addAttribute("user", user);
         return "details";
     }
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-        List<User> users = new ArrayList<>();
+        List<User> users = Arrays.asList(
+            new User("Pepa", "Gonzales"),
+            new User("Lalo", "Perez", "lalo@correo.com"),
+            new User("Juanita", "Bonita", "@correo.com"),
+            new User("Andres", "Hurtado")
+        );
 
         model.addAttribute("users", users);
         model.addAttribute("title", "Listado de usuarios!");
