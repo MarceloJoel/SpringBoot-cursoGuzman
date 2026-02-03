11dafd6e6b3533d8098abcc1624869d0b96011d0
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-02-03 18:06:37 -0500

    feat: usando @ModelAttribute

--- src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.curso.springboot.webapp.springboot_web.models.User;
 
@@ -25,17 +26,22 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-        List<User> users = Arrays.asList(
+        
+
+        // model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios!");
+
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+        return Arrays.asList(
             new User("Pepa", "Gonzales"),
             new User("Lalo", "Perez", "lalo@correo.com"),
             new User("Juanita", "Bonita", "@correo.com"),
             new User("Andres", "Hurtado")
         );
-
-        model.addAttribute("users", users);
-        model.addAttribute("title", "Listado de usuarios!");
-
-        return "list";
     }
 
 }
